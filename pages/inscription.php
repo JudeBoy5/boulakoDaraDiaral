f65c173ea1981628ae2519d1b10a4346878a5fac
PapaWeurseuckDieng <kirikoupapacha@gmail.com>
2025-02-27 18:41:45 +0000

    MAJ

--- pages/inscription.php
+++ pages/inscription.php
@@ -3,6 +3,7 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+	
 <head lang="en">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
